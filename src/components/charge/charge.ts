9320bc250c244a5a80a6475874e991f8d326bfaa
Jesus Bossa <x@jesusbossa.dev>
2021-01-17 17:09:11 -0500

    [ADD] Reading json string

--- src/components/charge/charge.ts
+++ src/components/charge/charge.ts
@@ -1,5 +1,19 @@
 import '../../styles/styles.css';
 
+function stringToJson(element: string) {
+  try {
+    console.log(JSON.parse(element));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function watchJson(element: any) {
+  stringToJson(element.target.value);
+}
+
 export default function Charge() {
-  console.log('charge');
+  const jota = document.getElementById('article-body-json');
+  jota.addEventListener('input', watchJson);
+  jota.addEventListener('change', watchJson);
 }
